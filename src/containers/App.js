e0ddab2cfadd071ba3a6d3fa3ae4cb3d17005f65
Tane Piper <piper.tane@gmail.com>
2016-04-18 19:02:23 +0100

    Output to input box, also copy to html button

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Panel, Input} from 'react-bootstrap';
+import { Navbar, Grid, Row, Col, Panel, Input} from 'react-bootstrap';
 import Dropzone from 'react-dropzone';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 function splitFile(data) {
-  return data.split('}').map(entry => {
+  return data.split('@article').filter(f => f).map(entry => {
+    console.log(entry)
     return entry.split('\n');
   });
 }
@@ -12,9 +14,25 @@ function getContentForKey(record, key) {
   const re = /= "(.+[^,|"])"/gi;
   let rows = record.filter(r => r.indexOf(key) !== -1);
   return rows.map(row => {
-    const text = re.exec(row);
-    return text[1] || '';
+    const textResults = re.exec(row);
+    let text = '';
+    if (textResults[1]) {
+      text = textResults[1]
+      .replace(/“/g, '"')
+      .replace(/”/g, '"')
+      .trim();
+    }
+    return text;
   });
+}
+
+function generateHTML(record) {
+  const htmlOutput = `
+    <div>
+      <h2>${getContentForKey(record, 'title')[0]}</h2>
+    </div>
+    `;
+    return htmlOutput;
 }
 
 export default class App extends Component {
@@ -39,16 +57,20 @@ export default class App extends Component {
     };
 
     files.forEach(file => {
-      reader.readAsBinaryString(file);
+      reader.readAsText(file);
     });
   }
 
+
+
   renderPanels() {
-    return this.state.records.map(record => {
-      console.log(getContentForKey(record, 'title')[0]);
+    return this.state.records.map((record, index) => {
+
+      const innerHtml = generateHTML(record);
       return (
-        <Panel>
-          <h2>{getContentForKey(record, 'title')[0]}</h2>
+        <Panel key={index} header={getContentForKey(record, 'title')[0]}>
+          <Input type="textarea" defaultValue={innerHtml} />
+          <CopyToClipboard text={innerHtml}><button>Copy to clipboard</button></CopyToClipboard>
         </Panel>
       );
     });
@@ -57,6 +79,13 @@ export default class App extends Component {
   render() {
     return (
       <Grid>
+        <Navbar staticTop inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+              .bib To Email Conveter
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
         <Row>
           <Col xs={6} xsOffset={3}>
             <Dropzone ref="dropzone" onDrop={this.onDrop.bind(this)}>
